5cf4e284850fd92ccc05bbe98d47bf377fef6867
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-03-02 21:31:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,20 @@
 from pyrogram import Client, filters
+import os
 
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token
-bot_token = '6991774790:AAFjhJM3Gi_aNYhpV6XQ6_zMNusu9F0CevU'
+# Retrieve your API ID, API hash, and bot token from environment variables
+api_id = int(os.environ.get("15191874"))
+api_hash = os.environ.get("3037d39233c6fad9b80d83bb8a339a07
+
+")
+bot_token = os.environ.get("6991774790:AAFjhJM3Gi_aNYhpV6XQ6_zMNusu9F0CevU")
 
 # Create a Pyrogram Client
-app = Client('my_user_id_bot', bot_token=bot_token)
+app = Client(
+    "my_user_id_bot",
+    api_id=api_id,
+    api_hash=api_hash,
+    bot_token=bot_token
+)
 
 # Define a handler for the /getid command
 @app.on_message(filters.command("getid", prefixes="/"))
